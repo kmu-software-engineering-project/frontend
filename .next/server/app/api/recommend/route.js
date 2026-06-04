4c4058570e814417517c4641f5985daf17a07714
chYoon1u <cyj0624@kookmin.ac.kr>
2026-06-04 22:45:15 +0900

    Remove .next and tsbuildinfo from repository

--- .next/server/app/api/recommend/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/recommend/route.js")
-R.c("server/chunks/[root-of-the-server]__0hsyv6_._.js")
-R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
-R.c("server/chunks/_next-internal_server_app_api_recommend_route_actions_0v.~yvy.js")
-R.m(40437)
-module.exports=R.m(40437).exports
